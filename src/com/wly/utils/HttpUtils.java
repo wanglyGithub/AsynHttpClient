294937ea506c1312b7b58ad2b5c5b52004288cbf
云中客 <wanglyGithub@users.noreply.github.com>
2016-09-02 15:10:17 +0800

    Update HttpUtils.java

--- src/com/wly/utils/HttpUtils.java
+++ src/com/wly/utils/HttpUtils.java
@@ -6,31 +6,37 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-
 import android.os.Handler;
-
+/**
+ * @date 2019/9/2
+ * @author wangly
+ *
+ */
 public class HttpUtils {
-	public static String get(String path, Handler handler){
+	public static String get(String path, Handler handler) {
 		try {
-			if(path!=null){
-				URL url=new URL(path);
-				HttpURLConnection conn=(HttpURLConnection) url.openConnection();
+			if (path != null && !"".equals(path)) {
+				URL url = new URL(path);
+				HttpURLConnection conn = (HttpURLConnection) url
+						.openConnection();
 				conn.setConnectTimeout(5000);
 				conn.setReadTimeout(1500);
 				conn.setRequestMethod("GET");
-				int code=conn.getResponseCode();
-				if(code==200){
-					InputStream is=conn.getInputStream();
+				int code = conn.getResponseCode();
+				if (code == 200) {
+					InputStream is = conn.getInputStream();
 					return StreamTools.readInputStream(is);
 				}
 			}
 		} catch (MalformedURLException e) {
-			handler.obtainMessage(AsyncHttpClient.NET_ERROR, "地址没有发现").sendToTarget();
+			handler.obtainMessage(Config.NET_ERROR, Config.ADDRESS_NOT_FOND)
+			.sendToTarget();
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-			handler.obtainMessage(AsyncHttpClient.NET_ERROR, "网络异常").sendToTarget();
+			handler.obtainMessage(Config.NET_ERROR, Config.NET_CONNECTED_ERROR)
+			.sendToTarget();
 		}
-		return null;
+		return "";
 	}
 }
