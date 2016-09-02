af01a75c5bb84b92b58e0bc5a0a1eb4ed3515904
云中客 <wanglyGithub@users.noreply.github.com>
2016-09-02 15:09:44 +0800

    Add files via upload

---
+++ src/com/wly/utils/Config.java
@@ -0,0 +1,19 @@
+package com.wly.utils;
+/**
+ * @create by data 2016/9/2
+ * @author wangly
+ *
+ */
+public class Config {
+	// -----Handler Message 值 Code
+	public static final int GET_SUCCESS = 102;  // 获取成功
+	public static final int GET_FAILED = 103;
+	public static final int NET_ERROR = 104;
+
+
+	//------控制网络
+	public static final String ADDRESS_NOT_FOND = "地址没有发现";
+	public static final String NET_CONNECTED_ERROR = "网络连接异常";
+	public static final String NOT_NETWORK_ERROR = "无网络";
+
+}
